fdd164561418f23450653a444af72e1cdf8993e8
DesarrolloTibs <cursos.capacitacion@tibs.com.mx>
2023-01-13 00:02:39 -0600

    Implementation Form Contacto Flex css y material ui

--- src/app/modules/contacts/components/form-contacts/form-contacts.component.ts
+++ src/app/modules/contacts/components/form-contacts/form-contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form-contacts',
   templateUrl: './form-contacts.component.html',
@@ -7,10 +8,12 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class FormContactsComponent implements OnInit {
   public id: string = "";
+  form: FormGroup = new FormGroup({});
 
   constructor(
     private route: ActivatedRoute,
-    public router: Router) {
+    public router: Router,
+    private fb: FormBuilder) {
 }
   ngOnInit(): void {
 
@@ -22,5 +25,16 @@ export class FormContactsComponent implements OnInit {
     const method = (this.id) ? 'patch' : 'post';
     console.log(this.id)
     console.log(method)
+    this.form = this.fb.group({
+      name: [null, [Validators.required, Validators.minLength(10)]],
+      email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      dob: [null, [Validators.required]],
+      address: [null],
+      country: [null],
+      gender: [null]
+    });
+  }
+  saveDetails(form: any) {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(form.value, null, 4));
   }
 }
